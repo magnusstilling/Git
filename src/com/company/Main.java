3f088df8e168e1f507ef244920707e82a5448a82
Magnus Stilling <magnust@ruc.dk>
2020-09-21 17:36:53 +0200

    tilføjet kommentar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //jeg prøver at skrive noget fedt her, så gør du ogs.
+        //en gang til prøver jeg sgu lige
     }
 }
